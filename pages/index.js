5ca6af6b034ccabc55edf70c283007aefc7c841a
Brad Traversy <traversymedia@gmail.com>
2021-06-24 12:57:52 -0400

    Sort by date

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import path from 'path'
 import matter from 'gray-matter'
 import Head from 'next/head'
 import Post from '../components/Post'
+import { sortByDate } from '../utils'
 
 export default function Home({ posts }) {
   return (
@@ -45,7 +46,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      posts,
+      posts: posts.sort(sortByDate),
     },
   }
 }
